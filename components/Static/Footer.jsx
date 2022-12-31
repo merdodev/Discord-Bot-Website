d1c53ea95f73beae38866642cba2744df309c29f
Merdo <103856523+merdodev@users.noreply.github.com>
2022-12-31 03:35:56 +0300

    Update Footer.jsx

--- components/Static/Footer.jsx
+++ components/Static/Footer.jsx
@@ -31,8 +31,8 @@ export default function Footer() {
                                     </Menu.Button>
 
 <div className="py-1"></div>
-<a href="https://top.gg/bot/924325576095973426">
-  <img src={"https://top.gg/api/widget/servers/924325576095973426.svg"}/>
+<a href="https://top.gg/bot/784214130202509333">
+  <img src={"https://top.gg/api/widget/servers/784214130202509333.svg"}/>
 </a>
 <div className="py-1"></div>
 <a href="https://top.gg/bot/924325576095973426">
